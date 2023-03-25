37fe3ada6e1991c79195d6163723e64fc1b3878d
이준용 <iiiuooyoo@gmail.com>
2023-03-26 01:33:33 +0900

    footer 하는 중

--- components/phone/screen/Footer.vue
+++ components/phone/screen/Footer.vue
@@ -2,7 +2,28 @@
     <div
         class="footer-container flex items-center justify-end flex-col h-full py-1"
     >
-        <div class="music-container flex flex-col"></div>
+        <div
+            class="music-container flex flex-col p-2 w-[94%] text-white items-center text-center rounded-2xl"
+        >
+            <div class="text-xs flex w-full p-1">
+                <div class="w-[40px] h-[40px] overflow-hidden rounded">
+                    <img
+                        src="~/assets/images/test.jpeg"
+                        alt=""
+                        class="w-full h-full object-cover"
+                    />
+                </div>
+                <div
+                    class="w-[60%] text-left px-3 flex flex-col justify-center"
+                >
+                    <p>등대</p>
+                    <p class="singer">하현상</p>
+                </div>
+                <div class="w-[20%]">이미지</div>
+            </div>
+            <div>호호</div>
+            <div class="flex"></div>
+        </div>
         <div class="flex items-center gap-x-1">
             <Icon
                 name="ic:baseline-circle"
@@ -21,9 +42,14 @@
 <script setup></script>
 
 <style lang="scss">
-/* .music-container{
-    background=
-} */
+.music-container {
+    transition: all 0.3s;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    .singer {
+        color: rgba(255, 255, 255, 0.5);
+    }
+}
 
 .footer-container {
     /* border: 1px solid #fff; */
