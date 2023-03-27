3b21c208637c6fdab40c3c4acffdcbf1129d310f
이준용 <iiiuooyoo@gmail.com>
2023-03-27 22:59:27 +0900

    feat : autoplay

--- components/phone/screen/Footer.vue
+++ components/phone/screen/Footer.vue
@@ -135,9 +135,12 @@ const emits = defineEmits(["setActive", "next", "prev"]);
 const deviceVolume = computed(() => props.volume);
 
 const audio = ref();
-const { playing, currentTime, duration, volume } = useMediaControls(audio, {
-    src: `/audios/${props.media.audio}`,
-});
+const { playing, currentTime, duration, volume, ended } = useMediaControls(
+    audio,
+    {
+        src: `/audios/${props.media.audio}`,
+    }
+);
 
 const startTime = computed(() =>
     dayjs().startOf("day").add(currentTime.value, "second").format("mm:ss")
@@ -146,7 +149,7 @@ const startTime = computed(() =>
 const endTime = computed(() =>
     dayjs()
         .startOf("day")
-        .add(Math.round(duration.value - currentTime.value), "second")
+        .add(Math.floor(duration.value - currentTime.value), "second")
         .format("mm:ss")
 );
 
@@ -156,21 +159,29 @@ const progressWidth = computed(() =>
     ((currentTime.value / duration.value) * 100).toFixed(2)
 );
 
-function mediaRest() {
+function mediaRest(autoPlay = false) {
     const audioConfig = useMediaControls(audio, {
         src: `/audios/${props.media.audio}`,
     });
 
-    playing.value = false;
     currentTime.value = audioConfig.currentTime.value;
     duration.value = audioConfig.duration.value;
-    volume.value = 1;
+
+    if (autoPlay) {
+        setTimeout(() => {
+            volume.value = 1;
+            playing.value = autoPlay;
+            console.log("왜 안하지");
+        }, 3000);
+    } else {
+        playing.value = false;
+    }
 }
 
-async function handleAudioChange(direction) {
+async function handleAudioChange(direction, autoPlay = false) {
     await emits(direction);
 
-    await mediaRest();
+    await mediaRest(autoPlay);
 }
 
 onMounted(() => {
@@ -186,6 +197,18 @@ watch(deviceVolume, (to, from) => {
 watch($progress, (to, from) => {
     currentTime.value = (duration.value / 100) * to;
 });
+
+watch(ended, (to, from) => {
+    if (to) {
+        handleAudioChange("next");
+    }
+});
+
+// watch(playing, (to, from) => {
+//     if (to && duration.value <= currentTime.value) {
+//         handleAudioChange("next", true);
+//     }
+// });
 </script>
 
 <style lang="scss">
