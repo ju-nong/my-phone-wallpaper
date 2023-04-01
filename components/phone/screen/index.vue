f86f195c8ded246d3dd4fa49fb6556dbc126d142
이준용 <iiiuooyoo@gmail.com>
2023-04-02 04:02:19 +0900

    feat : screen swipe distanceX 적용

--- components/phone/screen/index.vue
+++ components/phone/screen/index.vue
@@ -1,37 +1,44 @@
 <template>
-    <div
-        class="screen w-full overflow-hidden relative flex flex-col"
-        ref="$screen"
-    >
+    <div class="screen w-full overflow-hidden bg-black relative">
         <TransitionFade :duration="500">
             <div
                 v-if="!device.power"
                 class="off-screen w-full h-full left-0 top-0 absolute bg-black z-[100]"
             ></div>
         </TransitionFade>
-        <TransitionFade :duration="500">
-            <div
-                v-if="!audio.isFullScreen"
-                class="default-screen w-full h-full left-0 top-0 absolute z-[0] bg-center bg-repeat bg-cover"
-            ></div>
-        </TransitionFade>
-        <TransitionFade :duration="500">
-            <div
-                v-if="audio.isFullScreen"
-                class="active-screen w-full h-full left-0 top-0 absolute z-[1] bg-center"
-                :style="`background-image:url('/images/${audio.getAudio.cover}')`"
-            ></div>
-        </TransitionFade>
-        <PhoneScreenHeader />
-        <PhoneScreenMain />
-        <PhoneScreenBell />
-        <PhoneScreenVolume />
+        <div
+            v-if="isMain"
+            class="w-full h-full relative flex flex-col"
+            :style="`transform: translateX(-${screenLeft}px);`"
+            ref="$screen"
+        >
+            <TransitionFade :duration="500">
+                <div
+                    v-if="!audio.isFullScreen"
+                    class="default-screen w-full h-full left-0 top-0 absolute z-[0] bg-center bg-repeat bg-cover"
+                ></div>
+            </TransitionFade>
+            <TransitionFade :duration="500">
+                <div
+                    v-if="audio.isFullScreen"
+                    class="active-screen w-full h-full left-0 top-0 absolute z-[1] bg-center"
+                    :style="`background-image:url('/images/${audio.getAudio.cover}')`"
+                ></div>
+            </TransitionFade>
+            <PhoneScreenHeader />
+            <PhoneScreenMain />
+            <PhoneScreenBell />
+            <PhoneScreenVolume />
+        </div>
+        <div v-else class="w-fll h-full relative flex bg-orange-700 text-white">
+            카메라
+        </div>
     </div>
 </template>
 
 <script setup>
-import { TransitionFade } from "@morev/vue-transitions";
-import { usePointerSwipe } from "@vueuse/core";
+import { TransitionFade, TransitionSlide } from "@morev/vue-transitions";
+import { usePointerSwipe, useUserMedia } from "@vueuse/core";
 
 const device = useDeviceStore();
 const audio = useAudioStore();
@@ -39,16 +46,21 @@ const audio = useAudioStore();
 const $screen = ref();
 const { isSwiping, direction, distanceX } = usePointerSwipe($screen);
 
-watch(isSwiping, (to, from) => {
-    console.log(to);
-});
-
-watch(direction, (to, from) => {
-    console.log(to);
-});
+const isMain = ref(true);
+const screenLeft = ref();
 
 watch(distanceX, (to, from) => {
-    console.log(to);
+    screenLeft.value = to;
+});
+
+watch(isSwiping, (to, from) => {
+    if (!to) {
+        screenLeft.value = 0;
+
+        if (direction.value === "LEFT" && distanceX.value >= 50) {
+            isMain.value = !isMain.value;
+        }
+    }
 });
 </script>
 <style lang="scss">
